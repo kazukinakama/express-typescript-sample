c8ff90d5e3cea07376420c56f48377aa33ade950
kazukinakama <nakama.kazuki1215@gmail.com>
2022-08-24 23:43:36 +0900

    ライブラリをesbuildからts-jestに変更

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,6 @@
 module.exports = {
   transform: {
-    '^.+\\.ts?$': 'esbuild-jest',
+    '^.+\\.ts?$': 'ts-jest',
   },
-  clearMocks: true,
-  collectCoverage: false,
-  coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
   testMatch: ['**/tests/*.test.ts'],
 };
